fd1598371b1f0a7deb5a7f522f5f364bc530073c
fbeck <fbeck@student.42.fr>
2014-05-03 15:03:50 +0200

    adding OTRUNC to open - still buggy

--- srcs/ft_open_file.c
+++ srcs/ft_open_file.c
@@ -6,7 +6,7 @@
 /*   By: fbeck <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/30 19:48:12 by fbeck             #+#    #+#             */
-/*   Updated: 2014/05/01 10:58:48 by fbeck            ###   ########.fr       */
+/*   Updated: 2014/05/03 14:54:41 by fbeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int			ft_open_file(t_env *e)
 	if (e->op.a)
 		e->fd_file = open(path, O_WRONLY | O_CREAT | O_APPEND, 0666);
 	else
-		e->fd_file = open(path, O_WRONLY | O_CREAT, 0666);
+		e->fd_file = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (e->fd_file < 0)
 	{
 		printf("Failed to open file %s\n", path);
